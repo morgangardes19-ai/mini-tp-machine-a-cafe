19b98dd34c449550c10853ccc163a2b8e47143d0
morgan1000 <morgan.gardes.19@gmail.com>
2026-09-01 16:47:12 +0200

    màj

---
+++ machine-a-cafe.php
@@ -0,0 +1,11 @@
+<?php 
+class MachineACafe {
+    private string $marque;
+    private string $cafe;
+    private string $enFonction;
+
+    public function allumage() {}
+    public function faireDuCafe() {}
+    public function mettreUnDosette() {}
+}
+?>
