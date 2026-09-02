698c1e398ae79c0cd800dbc4b555879836c1a89f
morgan1000 <morgan.gardes.19@gmail.com>
2026-09-02 16:57:57 +0200

    finir le mécanisme de l'eau

--- machine-a-cafe.php
+++ machine-a-cafe.php
@@ -1,11 +1,81 @@
-<?php 
-class MachineACafe {
+<?php
+class MachineACafe
+{
     private string $marque;
-    private string $cafe;
-    private string $enFonction;
+    private bool $cafe;
+    private bool $estAllumé;
+    private int $eau;
 
-    public function allumage() {}
-    public function faireDuCafe() {}
-    public function mettreUnDosette() {}
+    public function __construct(string $marque)
+    {
+        $this->marque = $marque;
+        $this->estAllumé = false;
+        $this->cafe = false;
+        $this->eau = 0;
+    }
+
+    public function onOff()
+    {
+        if ($this->estAllumé) {
+            echo "La machine {$this->marque} s'éteint";
+            $this->estAllumé = false;
+        } else {
+            echo "La machine {$this->marque} s'allume";
+            $this->estAllumé = true;
+        }
+    }
+
+        public function remplirRecipiantEau(){
+        if ($this->eau === 0) {
+            echo "Rajoutez de l'eau dans le récipiant.";
+            return;
+        } else {
+            $this->eau === 500;
+        }
+    }
+
+    public function mettreUneDosette()
+    {
+        if (!$this->cafe) {
+            echo "La machine {$this->marque} n'a pas de dosette. On en ajoute une";
+            $this->cafe = true;
+        } else {
+            echo "Il y a déjà une dosette dans la machine.";
+        }
+    }
+
+    public function faireDuCafe()
+    {
+        if (!$this->estAllumé) {
+            echo "La machine est éteinte veuillez l'allumer.";
+            return;
+        }
+
+        // if ($this->eau >= 20) {
+            
+        // }
+
+        if (!$this->cafe) {
+            echo "Il n'y a pas de café, il faut en rajouter.";
+            return;
+        }
+        $this->cafe = false;
+        echo "Le café est prêt à être récupéré.";
+    }
 }
-?>
+
+
+$machine = new MachineACafe("senseo");
+var_dump($machine);
+
+// $machine->onOff();
+// var_dump($machine);
+
+$machine->remplirRecipiantEau();
+var_dump($machine);
+
+// $machine->mettreUneDosette();
+// var_dump($machine);
+
+// $machine->faireDuCafe();
+// var_dump($machine);
